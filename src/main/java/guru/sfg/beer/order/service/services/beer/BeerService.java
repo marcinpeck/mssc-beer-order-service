e66e39e65eacc385cd4570a417eeb7b6a486386b
marcinpeck <marcinpeck@gmail.com>
2021-04-12 18:04:42 +0200

    add beer information in beer order line FTO

---
+++ src/main/java/guru/sfg/beer/order/service/services/beer/BeerService.java
@@ -0,0 +1,14 @@
+package guru.sfg.beer.order.service.services.beer;
+
+import guru.sfg.beer.order.service.web.model.BeerDto;
+
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Created by marcin.peck on 12.04.2021
+ */
+public interface BeerService {
+    Optional<BeerDto> getBeerById(UUID id);
+    Optional<BeerDto> getBeerByUpc(String upc);
+}
